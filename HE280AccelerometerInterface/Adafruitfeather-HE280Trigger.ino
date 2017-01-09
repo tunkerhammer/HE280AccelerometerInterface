6f825db90c3c0a8ef43ce430c06a795ee77a8b14
tunkerhammer <martinisrael@sbcglobal.net>
2017-01-09 13:02:04 -0600

    Add files via upload

---
+++ HE280AccelerometerInterface/Adafruitfeather-HE280Trigger.ino
@@ -0,0 +1,99 @@
+//this forked from from Trash80 HE280AccelerometerInterface https://github.com/trash80/HE280AccelerometerInterface
+// by tunkerhammer 1/9/2017
+
+
+
+#include <Wire.h>
+#include <Adafruit_NeoPixel.h> // Comment out this line if NeoPixel are not used for lights 
+
+#define DEBUG 1 // Comment out this line to not echo results to serial
+
+//------------------------------------------------------------------------------------------------------
+#define BRIGHTNESS 30 // Comment out this line if NeoPixel are not used for lights  
+#define PIN 6 // Comment out this line if NeoPixel are not used for lights 
+#define NUM_LEDS 92 // Comment out this line if NeoPixel are not used for lights  
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRBW + NEO_KHZ800); // Comment out this line if NeoPixel are not used for lights 
+//------------------------------------------------------------------------------------------------------
+
+
+
+//------------------------------------------------------------------------------------------------------
+// this is setup for Adafruit Feather 32u4 Basic Proto or Adafruit Feather M0 Basic Proto
+const uint8_t click_pin = 11; // "INT" interrupt pin from HE280 - Blue on my model
+const uint8_t reset_pin = 12; // Used to re-send the initialization code to the HE280's accelerometer. Z Probe Mod pin.
+const uint8_t trigger_pin = 13; // Sends a HIGH signal when probe touches the table. Connected to Duet's Z probe in. Duet set to Type: 5.
+//------------------------------------------------------------------------------------------------------
+
+#define Z_PROBE_SENSITIVITY  14 // Seemecnc's default is 20, 15 is a good middle ground, 0-126 7 bit value
+
+#include "accelerometer.h"
+int debug_probe_disabled_switch = 0; //this is used to only send one proble disable for debug
+int delay_for_probe = 0; // this is used to have only one delay once in probe movement
+
+
+void setup() {
+  pinMode(click_pin, INPUT);
+  pinMode(trigger_pin, OUTPUT);
+  
+  #ifdef USE_RESET_PIN
+  pinMode(reset_pin, INPUT_PULLUP);
+  #endif
+  
+  while(!accelerometer_init()) {
+    debug_println("Failed Accel Init");
+    accelerometer_status();
+    digitalWrite(trigger_pin,HIGH); // Keep HIGH so that travel stops
+    delay(50);
+  }
+  digitalWrite(trigger_pin,LOW);
+  
+  //------------------------------------------------------------------------------------------------------
+  strip.setBrightness(BRIGHTNESS); // Comment out this line if NeoPixel are not used for lights 
+  strip.begin(); // Comment out this line if NeoPixel are not used for lights 
+  strip.show(); // Initialize all pixels to 'off'-- Comment out this line if NeoPixel are not used for lights 
+  colorWipe(strip.Color(255, 0, 0), 50); // Red -- Comment out this line if NeoPixel are not used for lights 
+  colorWipe(strip.Color(0, 255, 0), 50); // Green -- Comment out this line if NeoPixel are not used for lights 
+  colorWipe(strip.Color(0, 0, 255), 50); // Blue -- Comment out this line if NeoPixel are not used for lights 
+  colorWipe(strip.Color(0, 0, 0, 255), 80); // White-- Comment out this line to not use neopixs for lights
+  //------------------------------------------------------------------------------------------------------ 
+}
+
+void loop() {
+    //debug_println (xx);
+    if(digitalRead(reset_pin)) {
+        if (delay_for_probe == 0){  //only need to delay enableing probe once
+          debug_println("enable probe");
+          delay(60); //delay used to give the printer time to start to move before enableing probe
+          delay_for_probe = 1;         
+        }
+
+        if(!digitalRead(click_pin)) {
+            digitalWrite(trigger_pin,HIGH);
+            delay(100);
+            digitalWrite(trigger_pin,LOW);
+            debug_print("Accel hit: ");
+            debug_println(accelerometer_read(0x31)); 
+            debug_probe_disabled_switch = 0;
+            delay_for_probe = 0;     
+        }
+    }
+        
+    if(!digitalRead(reset_pin)) {
+        if (debug_probe_disabled_switch == 0){
+          debug_probe_disabled_switch =1;
+          debug_println("disable probe");
+        }
+        //digitalWrite(trigger_pin,LOW);
+    }
+
+
+}
+
+// Comment out the following if NeoPixel are not used for lights 
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=24; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
